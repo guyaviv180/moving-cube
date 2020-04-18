4d85e02aab43b3c01e448a7b6cfd56125e493a72
guy aviv <guyaviv180@gmail.com>
2020-04-18 21:51:18 +0300

    Add project files.

---
+++ moving-cube/graphics.js
@@ -0,0 +1,33 @@
+﻿function drawRectangle(x, y, width, height, color, borderWidth, borderColor) {
+    context.beginPath();
+    context.rect(x, y, width, height);
+    context.fillStyle = color;
+    context.fill();
+    context.lineWidth = borderWidth;
+    context.strokeStyle = borderColor;
+    context.stroke();
+}
+
+function drawArc(x, y, r, startAngle, endAngle, counterClockWise, color, borderWidth, borderColor) {
+    context.beginPath();
+    context.arc(x, y, r, startAngle, endAngle, counterClockWise);
+    context.fillStyle = color;
+    context.fill();
+    context.lineWidth = borderWidth;
+    context.strokeStyle = borderColor;
+    context.stroke();
+
+}
+
+function drawTriangle() {
+    context.beginPath();
+    context.moveTo(xOfFirstPoint, yOfFirstPoint);
+    context.lineTo(xOfSecondPoint, yOfSecondPoint);
+    context.lineTo(xOFThirdPoint, yOFThirdPoint);
+    context.lineTo(xOfFirstPoint, yOfFirstPoint);
+    context.fillStyle = color;
+    context.fill();
+    context.lineWidth = borderWidth;
+    context.strokeStyle = borderColor;
+    context.stroke();
+}
